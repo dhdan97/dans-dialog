3ca750990a8e10ee621015e9113452064e3927b2
Daniel Hernandez <dhdan97@gmail.com>
2023-03-30 22:31:57 -0400

    Connected to contentful succesfully

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,24 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: "Dan\'s Dialog",
+    title: "Dan's Dialog",
     author: "Daniel Hernandez",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+  ],
 };
